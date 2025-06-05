bb97e4bfed2b802db7b7ebb2e79b168eb8565c9e
matu.dev <mati.a.gimenez2002@gmail.com>
2025-06-05 16:41:27 -0300

    clase 23

--- src/environment.js
+++ src/environment.js
@@ -12,6 +12,7 @@ export const ENVIRONMENT = {
     PORT: process.env.PORT,
     GMAIL_PASSWORD: process.env.GMAIL_PASSWORD,
     GMAIL_USERNAME: process.env.GMAIL_USERNAME,
-    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY
+    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+    DB_MONGO_PASSWORD: process.env.DB_MONGO_PASSWORD
 }
 
